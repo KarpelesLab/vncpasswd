1be85489e7a0488ab0a5590b2fecef2a2e8d7f2d
Mark Karpeles <magicaltux@gmail.com>
2020-07-08 21:14:05 +0900

    update infog

--- func.go
+++ func.go
@@ -1,11 +1,6 @@
 package vncpasswd
 
-// two password functions for VNC protocol
-func DecryptPasswd(data []byte) []byte {
-	dk := deskey(vnckey, true)
-	return desfunc(data, dk)
-}
-
+// GenerateResponse is used for VNC auth and is provided as is from the python implementation
 func GenerateResponse(passwd, challenge []byte) []byte {
 	var key [8]byte
 	copy(key[:], passwd)
@@ -13,6 +8,7 @@ func GenerateResponse(passwd, challenge []byte) []byte {
 	return append(desfunc(challenge[:8], ek), desfunc(challenge[8:], ek)...)
 }
 
+// Crypt will encrypt the given password in format expected by VNC
 func Crypt(password string) []byte {
 	pwd := make([]byte, 8)
 	copy(pwd, password)
@@ -21,6 +17,7 @@ func Crypt(password string) []byte {
 	return desfunc(pwd, key)
 }
 
+// Decrypt will decrypt and return a VNC password
 func Decrypt(value []byte) string {
 	key := deskey(vnckey, true)
 	return string(desfunc(value, key))
